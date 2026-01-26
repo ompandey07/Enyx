f70ab278586a3351882e72190a3f6a7fdf3b97a3
ompandey07 <iamompadney.it@gmail.com>
2026-01-26 10:57:04 +0545

    Sh update

--- run_docker.sh
+++ run_docker.sh
@@ -3,12 +3,17 @@
 docker stop exptrac_app || true
 docker rm exptrac_app || true
 
-docker build --no-cache -t exptrac .
+# Build only once (no --no-cache)
+docker build -t exptrac .
 
+# Run container, mount code for fast reload
 docker run -d \
   --name exptrac_app \
   --network host \
+  --restart unless-stopped \
+  -v $(pwd):/app \
   exptrac
 
+# Run migrations
 docker exec -it exptrac_app python manage.py makemigrations
 docker exec -it exptrac_app python manage.py migrate
